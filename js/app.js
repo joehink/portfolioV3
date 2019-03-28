04bfb660e99f0a06a6f6a32ca3e0d938d28ad07e
Joe Hinkley <joehhinkley@gmail.com>
2019-03-27 23:36:12 -0700

    added landing animations and mobile styling

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 window.onload = function() {
     anime({
+        targets: '#landing',
+        translateY: [-1000, 0],
+        opacity: [0, 1],
+        duration: 1500,
+        easing: 'cubicBezier(.54,0,.54,1.01)'
+    })
+    anime({
         targets: 'i',
-        translateX: [-1000,0],
-        autoplay: true,
-        delay: anime.stagger(100), 
+        scale: [0, 1],
+        duration: 500,
+        delay: anime.stagger(200, { start: 1500 }), 
         easing: 'easeInOutQuad'
     })
 }
