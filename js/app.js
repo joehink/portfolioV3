8f582ba0a93b88753e235fff368966c557a41ab3
Joe Hinkley <joehhinkley@gmail.com>
2019-03-28 19:21:18 -0700

    built slider

--- js/app.js
+++ js/app.js
@@ -1,24 +1,59 @@
-const createSlideShow = (elem, urls) => {
+const createSlideShow = (id, urls) => {
+    const elem = document.getElementById(id)
     let index = 0;
-    const next = (display) => {
-        if (index === urls.length - 1) {
-            index = 0;
-            display.src = urls[index];
-        } else {
+    let animationInProgress = false;
+    const next = (display, displayContainer, queue) => {
+        if (index < urls.length - 1 && !animationInProgress) {
+            animationInProgress = true;
             index += 1;
-            display.src = urls[index];
+            display.classList.add("slide-left");
+
+            [...queue.children].forEach(child => child.classList.remove("current"));
+
+            queue.children[index].classList.add("current");
+
+            queue.children[index].scrollIntoView({behavior: "smooth", block: "nearest"});
+
+
+            const nextDisplay = document.createElement('img');
+            nextDisplay.classList.add("slide-left");
+            nextDisplay.src = urls[index];
+            displayContainer.append(nextDisplay);
+            setTimeout(() => {
+                nextDisplay.remove();
+                display.classList.remove("slide-left")
+                display.src = urls[index];
+                animationInProgress = false;
+            }, 500)
         }
     }
-    const back = (display) => {
-        if (index === 0) {
-            index = urls.length - 1;
-            display.src = urls[index];
-        } else {
+    const back = (display, displayContainer, queue) => {
+        if (index > 0 && !animationInProgress) {
+            animationInProgress = true;
             index -= 1;
-            display.src = urls[index];
+            display.classList.add("slide-right");
+
+            [...queue.children].forEach(child => child.classList.remove("current"));
+
+            queue.children[index].classList.add("current");
+
+            queue.children[index].scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"});
+
+            const prevDisplay = document.createElement('img');
+            prevDisplay.classList.add("slide-right");
+            prevDisplay.src = urls[index];
+            displayContainer.prepend(prevDisplay);
+            setTimeout(() => {
+                prevDisplay.remove();
+                display.classList.remove("slide-right")
+                display.src = urls[index];
+                animationInProgress = false;
+            }, 500)
         }
     }
+
     const displayContainer = document.createElement('div');
+    displayContainer.classList.add("display-container");
     const display = document.createElement('img');
     display.src = urls[index];
     display.classList.add("display");
@@ -27,22 +62,25 @@ const createSlideShow = (elem, urls) => {
     const queue = document.createElement('div');
     queue.classList.add("queue");
     
-    urls.forEach(url => {
+    urls.forEach((url, urlIndex) => {
         const image = document.createElement('img') ;
         image.src = url
         image.classList.add("in-queue")
+        if (urlIndex === index) {
+            image.classList.add("current")
+        }
         queue.append(image)
     })
 
     const backArrowContainer = document.createElement('div');
-    backArrowContainer.classList.add("backArrow-container");
+    backArrowContainer.classList.add("back", "arrow-container");
     backArrowContainer.innerHTML = '<i class="fas fa-chevron-left"></i>';
-    backArrowContainer.addEventListener("click", () => back(display));
+    backArrowContainer.addEventListener("click", () => back(display, displayContainer, queue));
 
     const nextArrowContainer = document.createElement('div');
-    nextArrowContainer.classList.add("nextArrow-container");
+    nextArrowContainer.classList.add("next", "arrow-container");
     nextArrowContainer.innerHTML = '<i class="fas fa-chevron-right"></i>';
-    nextArrowContainer.addEventListener("click", () => next(display));
+    nextArrowContainer.addEventListener("click", () => next(display, displayContainer, queue));
 
     displayContainer.append(backArrowContainer);
     displayContainer.append(nextArrowContainer);
@@ -55,8 +93,7 @@ const createSlideShow = (elem, urls) => {
 window.onload = function() {
     new Rellax('.rellax');
 
-    const project = document.querySelector('.slider')
-    createSlideShow(project, ["./images/circuitBreaker/1.png", "./images/circuitBreaker/2.png"])
+    createSlideShow('circuit-breaker-slider', ["./images/circuitBreaker/1.png", "./images/circuitBreaker/2.png", "./images/circuitBreaker/1.png", "./images/circuitBreaker/2.png", "./images/circuitBreaker/1.png", "./images/circuitBreaker/2.png"])
 
     anime({
         targets: '#landing .container',
